a5ac4bc3c76e53da430c15ec621d989262fddac6
milap468 <milap.c@ahduni.edu.in>
2024-03-21 00:33:28 +0530

    fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import UploadWidget from './UploadWidget'
 import { axiosApi } from '../api/axios';
 import Cookies from 'js-cookie';
-
+import logo from '../assets/logo.png';
 
 function Profile() {
 
@@ -13,6 +13,7 @@ function Profile() {
     const [type, setType] = useState('');
     const [error, setError] = useState(false);
     const [msgToken, setMsgToken] = useState("")
+    const [open,setOpen] = useState(true)
 
     useEffect(function () {
 
@@ -94,6 +95,10 @@ function Profile() {
         }
     }
 
+    function closeBanner(){
+        setOpen(false)
+    }
+
 
 
 
@@ -125,24 +130,26 @@ function Profile() {
                 </div>
             </div> : ""}
 
-            <div id="marketing-banner" tabindex="-1" class="fixed z-50 flex flex-col md:flex-row justify-between w-[calc(100%-2rem)] p-4 -translate-x-1/2 bg-white border border-gray-100 rounded-lg shadow-sm lg:max-w-7xl left-1/2 top-6 dark:bg-gray-700 dark:border-gray-600">
-                <div class="flex flex-col items-start mb-3 me-4 md:items-center md:flex-row md:mb-0">
-                    <a href="https://flowbite.com/" class="flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:border-e md:mb-0 dark:border-gray-600">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 me-2" alt="Flowbite Logo" />
-                        <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            {
+                open ? <div id="marketing-banner" tabindex="-1" className="fixed mt-14 z-50 flex flex-col md:flex-row justify-between w-[calc(100%-2rem)] p-4 -translate-x-1/2 bg-white border border-gray-100 rounded-lg shadow-sm lg:max-w-7xl left-1/2 top-6 dark:bg-gray-700 dark:border-gray-600">
+                <div className="flex flex-col items-start mb-3 me-4 md:items-center md:flex-row md:mb-0">
+                    <a href="https://flowbite.com/" className="flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:border-e md:mb-0 dark:border-gray-600">
+                        <img src={logo} className="h-6 me-2" alt="Flowbite Logo" />
+                        <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
                     </a>
-                    <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">Build websites even faster with components on top of Tailwind CSS</p>
+                    <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">we encourage you to revoke your token after your uploading is done and when you have required then regenerate it by clicking credentials</p>
                 </div>
-                <div class="flex items-center flex-shrink-0">
-                    <a href="#" class="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</a>
-                    <button data-dismiss-target="#marketing-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <div className="flex items-center flex-shrink-0">
+                   
+                    <button onClick={closeBanner} data-dismiss-target="#marketing-banner" type="button" className="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                         </svg>
-                        <span class="sr-only">Close banner</span>
+                        <span  className="sr-only">Close banner</span>
                     </button>
                 </div>
-            </div>
+            </div> : ""
+            }
 
 
             <div className='flex flex-col items-center mt-16' >
