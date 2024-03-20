51db2bc09caa1e9d6f8ed565c56245b659863243
milap468 <milap.c@ahduni.edu.in>
2024-03-20 21:25:46 +0530

    added

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PrivacyPolicy() {
     return (
-        <div>
+        <div className='text-white m-4'>
             <h1>Privacy Policy</h1>
             <p>Last updated: March 20, 2024</p>
             <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
