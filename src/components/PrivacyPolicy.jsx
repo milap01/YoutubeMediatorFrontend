f1b9b4940f453169da57104da84554d020e715b0
milap468 <milap.c@ahduni.edu.in>
2024-03-20 21:42:13 +0530

    adde

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 function PrivacyPolicy() {
     return (
         <div className='text-white m-4'>
-            <h1 className='text-4xl text-bold'>Privacy Policy</h1>
+            <h1 className='text-6xl text-bold'>Privacy Policy</h1>
             <p className='mt-4'>Last updated: March 20, 2024</p>
 
             <p className='mt-2'>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
 
             <p className='mt-2'>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/" target="_blank">Free Privacy Policy Generator</a>.</p>
 
-            <h2 className=' text-2xl text-bold mt-4'>Interpretation and Definitions</h2>
-            <h3 className=' text-lg mt-2' >Interpretation</h3>
+            <h2 className=' text-3xl text-bold mt-4'>Interpretation and Definitions</h2>
+            <h3 className=' text-xl mt-2 text-bold' >Interpretation</h3>
             <p className='mt-2'>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
 
-            <h3 className='text-lg mt-2'>Definitions</h3>
-            <p className=' mt-2'>For the purposes of this Privacy Policy:</p>
-            <ul>
-                <li>
+            <h3 className='text-xl mt-2 text-bold'>Definitions</h3>
+            <p className=' mt-2 '>For the purposes of this Privacy Policy:</p>
+            <ul className='mt-2 list-disc'>
+                <li className=''>
                     <p><strong>Account</strong> means a unique account created for You to access our Service or parts of our Service.</p>
                 </li>
                 <li>
