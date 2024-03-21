14166ebfa5f9308d9273e5d438fc1b1bab06281d
milap468 <milap.c@ahduni.edu.in>
2024-03-21 22:46:38 +0530

    adde

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import { axiosApi } from '../api/axios';
 import Cookies from 'js-cookie';
 import { AES, enc } from 'crypto-js';
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
