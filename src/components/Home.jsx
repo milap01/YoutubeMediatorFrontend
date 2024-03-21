cc8ce7acd5b1ae4803abfac09e2d125082f3226d
milap468 <milap.c@ahduni.edu.in>
2024-03-21 23:25:55 +0530

    addde

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -524,7 +524,7 @@ function Home() {
                   <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={user.avatar ? user.avatar : logo} alt="Bonnie image" />
                   <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white font-mono">{user.full_name}</h5>
                   <span className="text-sm text-gray-500 dark:text-gray-400 font-mono ">{user.type}</span>
-                  <div className="flex mt-4 md:mt-6">
+                  <div className="flex mt-4 p-2 md:mt-6">
                     {
                       <button id={user.email} onClick={(e) => handelFriendRequest(user.email)} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800 font-mono ">Add Friend</button>
                     }
