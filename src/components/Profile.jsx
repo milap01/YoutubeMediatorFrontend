20f909a9ddad6ea80233088583580cf024e742bc
milap468 <milap.c@ahduni.edu.in>
2024-03-22 23:58:18 +0530

    added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -134,24 +134,24 @@ function Profile() {
             </div> : ""}
 
             {
-                open ? <div id="marketing-banner" tabindex="-1" class="fixed mt-24 mb-10 z-50 flex flex-col md:flex-row justify-between w-full md:max-w-7xl p-4 md:px-8 bg-white border border-gray-100 rounded-lg shadow-sm md:left-1/2 md:-translate-x-1/2 md:top-6 dark:bg-gray-700 dark:border-gray-600">
-                <div class="flex flex-col items-start mb-3 md:mb-0">
-                    <a href="https://flowbite.com/" class="flex items-center mb-2 border-gray-200 md:border-e md:mb-0 dark:border-gray-600">
-                        <img src={logo} class="h-6 me-2" alt="Flowbite Logo" />
-                        <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">YoutubeMediator</span>
+                open ? <div id="marketing-banner" tabindex="-1" className="fixed mt-24 mb-10 z-50 flex flex-col md:flex-row justify-between w-[calc(100%-2rem)] p-4 -translate-x-1/2 bg-white border border-gray-100 rounded-lg shadow-sm lg:max-w-7xl left-1/2 top-6 dark:bg-gray-700 dark:border-gray-600">
+                <div className="flex flex-col items-start mb-3 me-4 md:items-center md:flex-row md:mb-0">
+                    <a href="https://flowbite.com/" className="flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:border-e md:mb-0 dark:border-gray-600">
+                        <img src={logo} className="h-6 me-2" alt="Flowbite Logo" />
+                        <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">YoutubeMediator</span>
                     </a>
-                    <p class="text-sm font-normal text-gray-500 dark:text-gray-400 md:ml-4 md:pl-4">we encourage you to revoke your token after your uploading is done and when you have required then regenerate it by clicking credentials</p>
+                    <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">we encourage you to revoke your token after your uploading is done and when you have required then regenerate it by clicking credentials</p>
                 </div>
-                <div class="flex items-center">
-                    <button onclick={closeBanner} data-dismiss-target="#marketing-banner" type="button" class="inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg p-1.5 dark:hover:bg-gray-600 dark:hover:text-white focus:outline-none focus:ring focus:ring-gray-300">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <div className="flex items-center flex-shrink-0">
+                   
+                    <button onClick={closeBanner} data-dismiss-target="#marketing-banner" type="button" className="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                         </svg>
-                        <span class="sr-only">Close banner</span>
+                        <span  className="sr-only">Close banner</span>
                     </button>
                 </div>
-            </div>
-             : ""
+            </div> : ""
             }
 
 
