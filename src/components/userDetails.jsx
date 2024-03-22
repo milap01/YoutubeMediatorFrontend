296eb2bda21508a59255dd97f2eccea192c5188d
milap468 <milap.c@ahduni.edu.in>
2024-03-22 23:27:27 +0530

    added

--- src/components/userDetails.jsx
+++ src/components/userDetails.jsx
@@ -73,7 +73,7 @@ function UserDetails() {
             </div>
 
 
-            <p className='text-white text-center font-mono  m-8 mt-4 text-wrap'>
+            <p className='text-white text-sm md:text-base lg:text-lg xl:text-xl leading-relaxed'>
 
                 {user.bio}
 
